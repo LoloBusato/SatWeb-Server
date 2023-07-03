916f96364d2f2d6ceb9c6148dcaeb8e7ad1e4fba
Lorenzo Busato <lorenzobusato07@gmail.com>
2023-07-03 12:15:34 -0300

    Add: agrego un metodo por distribute en el que se obtiene todas las sucursales que tienen un repuesto en particular

--- CRUD/stock.js
+++ CRUD/stock.js
@@ -54,6 +54,17 @@ router.post("/", (req, res) => {
       return res.status(200).json(data);
     });
   })
+  router.get("/distribute/:id", (req, res) => {
+    const branchId = req.params.id;
+    const qgetStock = "SELECT * FROM stock JOIN repuestos ON stock.repuesto_id = repuestos.idrepuestos JOIN proveedores ON stock.proveedor_id = proveedores.idproveedores JOIN stockbranch ON stock.idstock = stockbranch.stock_id WHERE stockbranch.stock_id = ?";
+    db.query(qgetStock, [branchId], (err, data) => {
+      if (err) {
+        console.log(err);
+        return res.status(400).json(err);
+      }
+      return res.status(200).json(data);
+    });
+  })
   // update
   router.put("/:id", (req, res) => {
     const stockId = req.params.id;
