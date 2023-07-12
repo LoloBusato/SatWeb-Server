1a0910b374a6044e3b0aec464e66c2fb8f6754a0
Lorenzo Busato <lorenzobusato07@gmail.com>
2023-07-12 15:18:47 -0300

    Modify: cambio el post del archivo reduceStock ya que tiene que modificar el valor en la tabla stockbranch

--- CRUD/reduceStock.js
+++ CRUD/reduceStock.js
@@ -6,7 +6,7 @@ const db = require('../database/dbConfig');
 /*-----------------CREACION DE SISTEMA DE REPUESTOS USADOS----------------- */
 router.post("/", (req, res) => {
     const qInsertReduceStock = "INSERT INTO reducestock (orderid, userid, stockid, date) VALUES (?, ?, ?, ?)";
-    const qupdateStock = "UPDATE stock SET `cantidad` = ? WHERE idstock = ?";
+    const qupdateStock = "UPDATE stockbranch SET `cantidad_restante` = ? WHERE stockbranchid = ?";
   
     const { orderId, userId, stockId, cantidad, fecha } = req.body;
     // Obtener la fecha y hora actual
@@ -31,19 +31,8 @@ router.post("/", (req, res) => {
     const qgetStock = "SELECT idreducestock, orderid, idstock, cantidad, repuesto, precio_compra, nombre, username, reducestock.date  FROM reducestock JOIN users ON reducestock.userid = users.idusers JOIN stock ON reducestock.stockid = stock.idstock JOIN repuestos ON stock.repuesto_id = repuestos.idrepuestos JOIN proveedores ON stock.proveedor_id = proveedores.idproveedores ORDER BY STR_TO_DATE(reducestock.date, '%d/%m/%y') DESC";
     db.query(qgetStock, (err, data) => {
       if (err) {
-        console.log(err);
-        return res.status(400).json("error al obtener la lista de Stock");
+        return res.status(400).json(err);
       }
-      return res.status(200).json(data);
-    });
-  })
-  router.put("/:id", (req, res) => {
-    const qupdateStock = "UPDATE stock SET `cantidad` = ? WHERE idstock = ?";
-    const stockId = req.params.id;
-    const { cantidad } = req.body;
-    
-    db.query(qupdateStock, [cantidad,stockId], (err, data) => {
-      if (err) return res.status(400).send(err);
       return res.status(200).json(data);
     });
   })
