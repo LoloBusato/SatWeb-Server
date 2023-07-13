ed978d744a3b4a98f23fad62a20a08203471027d
Lorenzo Busato <lorenzobusato07@gmail.com>
2023-07-13 14:13:31 -0300

    Update: cambio la manera de agregar repuestos para que no dependa del stcok sino de la tabla intermedia

--- CRUD/reduceStock.js
+++ CRUD/reduceStock.js
@@ -5,45 +5,52 @@ const router = express.Router();
 const db = require('../database/dbConfig');
 /*-----------------CREACION DE SISTEMA DE REPUESTOS USADOS----------------- */
 router.post("/", (req, res) => {
-    const qInsertReduceStock = "INSERT INTO reducestock (orderid, userid, stockid, date) VALUES (?, ?, ?, ?)";
-    const qupdateStock = "UPDATE stockbranch SET `cantidad_restante` = ? WHERE stockbranchid = ?";
-  
-    const { orderId, userId, stockId, cantidad, fecha } = req.body;
-    // Obtener la fecha y hora actual
-  
-    const values = [
-      orderId,
-      userId,
-      stockId,
-      fecha,
-    ]
-    db.query(qupdateStock, [cantidad,stockId], (err, data) => {
+  const qupdateStock = "UPDATE stockbranch SET `cantidad_restante` = ? WHERE stockbranchid = ?";
+  const qInsertReduceStock = "INSERT INTO reducestock (orderid, userid, stockid, date) VALUES (?, ?, ?, ?)";
+
+  const { orderId, userId, stockbranchid, cantidad, fecha } = req.body;
+
+  const values = [
+  orderId,
+  userId,
+  stockbranchid,
+  fecha,
+  ]
+  db.query(qupdateStock, [cantidad,stockbranchid], (err, data) => {
+    if (err) return res.status(400).send(err);
+
+    db.query(qInsertReduceStock, values, (err, data) => {
       if (err) return res.status(400).send(err);
-      
-      db.query(qInsertReduceStock, values, (err, data) => {
-        if (err) return res.status(400).send(err);
-        return res.status(200).send(data);
-      });
+      return res.status(200).send(data);
     });
-  
-  })// read
-  router.get("/", (req, res) => {
-    const qgetStock = "SELECT idreducestock, orderid, idstock, cantidad, repuesto, precio_compra, nombre, username, reducestock.date  FROM reducestock JOIN users ON reducestock.userid = users.idusers JOIN stock ON reducestock.stockid = stock.idstock JOIN repuestos ON stock.repuesto_id = repuestos.idrepuestos JOIN proveedores ON stock.proveedor_id = proveedores.idproveedores ORDER BY STR_TO_DATE(reducestock.date, '%d/%m/%y') DESC";
-    db.query(qgetStock, (err, data) => {
-      if (err) {
-        return res.status(400).json(err);
-      }
-      return res.status(200).json(data);
-    });
-  })
-  router.delete("/:id", (req, res) => {
-    const stockReduceId = req.params.id;
-    const qdeleteStock = " DELETE FROM reducestock WHERE idreducestock = ? ";
-  
+  });
+})
+// read
+router.get("/:id", (req, res) => {
+  const stockId = req.params.id;
+  const qgetStock = "SELECT idreducestock, orderid, reducestock.date, username, stockbranchid, cantidad_restante, idstock, repuesto, nombre FROM reducestock JOIN users ON reducestock.userid = users.idusers JOIN stockbranch ON reducestock.stockid = stockbranch.stockbranchid JOIN stock ON stockbranch.stock_id = stock.idstock JOIN repuestos ON stock.repuesto_id = repuestos.idrepuestos JOIN proveedores ON stock.proveedor_id = proveedores.idproveedores WHERE orderid = ? ORDER BY STR_TO_DATE(reducestock.date, '%d/%m/%y') DESC";
+  db.query(qgetStock, [stockId], (err, data) => {
+    if (err) {
+      return res.status(400).json(err);
+    }
+    return res.status(200).json(data);
+  });
+})
+router.delete("/:id", (req, res) => {
+  const stockReduceId = req.params.id;
+  const qdeleteStock = "DELETE FROM reducestock WHERE idreducestock = ?";
+
+  const qupdateStock = "UPDATE stockbranch SET `cantidad_restante` = ? WHERE stockbranchid = ?";
+  const { cantidad, stockbranchid } = req.body;
+
+  db.query(qupdateStock, [cantidad,stockbranchid], (err, data) => {
+    if (err) return res.status(400).send(err);
+
     db.query(qdeleteStock, [stockReduceId], (err, data) => {
       if (err) return res.status(400).send(err);
       return res.status(200).json(data);
     });
-  })
+  });
+})
 
-  module.exports = router
+module.exports = router
