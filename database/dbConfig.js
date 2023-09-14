bc3fba315c9cebd73f4b4c343869f13c46bb9592
Lorenzo Busato <lorenzobusato07@gmail.com>
2023-09-14 12:47:18 -0300

    Add: agrego un metodo get a los cobros

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DBNAME,
-  connectionLimit: 20,
+  connectionLimit: 5,
 });
 
 module.exports = pool;
