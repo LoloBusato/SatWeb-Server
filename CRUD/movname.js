00cbec6d9c17bd5ee4d3adbdcd91fa7301d77149
Lorenzo Busato <lorenzobusato07@gmail.com>
2023-09-14 12:16:56 -0300

    Add: agrego una funcion post para movesRepairs en movname.js

--- CRUD/movname.js
+++ CRUD/movname.js
@@ -112,6 +112,86 @@ router.post('/movesSells', async (req, res) => {
       })
     })
   });
+router.post('/movesRepairs', async (req, res) => {
+    const { 
+      ingreso,
+      egreso,
+      operacion, 
+      monto,
+      userId,
+      branch_id,
+      fecha,
+      order_id,
+      arrayMovements,
+      cobrosValues,
+      entregarOrden,
+    } = req.body;
+
+    const valuesCreateMovename = [
+      ingreso,
+      egreso,
+      operacion,
+      monto,
+      fecha,
+      userId,
+      branch_id,
+      order_id,
+    ]
+    const qCreateMoveName= "INSERT INTO movname (ingreso, egreso, operacion, monto, fecha, userId, branch_id, order_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+    const qCreateMovement = "INSERT INTO movements (movcategories_id, unidades, movname_id, branch_id) VALUES (?, ?, ?, ?)";
+
+    const qCreateCobros = "INSERT INTO cobros (order_id, fecha, movname_id, pesos, dolares, banco, mercado_pago, encargado) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+
+    async function executeTransaction() {
+
+      const db = await pool.promise().getConnection();
+      try {
+        await db.beginTransaction();
+
+        // Insertar el movname
+        const [insertMovnameResult] = await db.execute(qCreateMoveName, valuesCreateMovename);
+        const moveName_id = insertMovnameResult.insertId;
+  
+        // Insertar los movimientos
+        await Promise.all(arrayMovements.map(async (element) => {
+          await db.execute(qCreateMovement, [...element, moveName_id, branch_id]);
+        }));
+  
+        // Insertar cobros
+        const cobrosValuesArr = [
+          order_id,
+          fecha,
+          moveName_id,
+          cobrosValues.pesos || 0,
+          cobrosValues.dolares || 0,
+          cobrosValues.banco || 0,
+          cobrosValues.mercado_pago || 0,
+          cobrosValues.encargado || 0,
+        ];
+        await db.execute(qCreateCobros, cobrosValuesArr);
+  
+        // Reasignar la orden como entregada
+        if (entregarOrden) {
+          const qupdateOrder = "UPDATE orders SET `returned_at` = ?, `state_id` = 6, `users_id` = 18 WHERE order_id = ?";
+          await db.execute(qupdateOrder, [fecha.split(' ')[0], order_id]);
+        }
+  
+        // Commit si todo fue exitoso
+        await db.commit();
+        return res.status(200).send('Repuesto agregado con éxito');
+
+      } catch (err) {
+        await db.rollback();
+        console.error(err)
+        return res.status(500).send(err);
+
+      } finally {
+        db.release();
+      }
+    }
+    executeTransaction()
+  });
   // read
   router.get("/:id", (req, res) => {
     const moveId = req.params.id;
